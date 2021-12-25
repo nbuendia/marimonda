d2adca5b76c4960e4435dafd2f6e2004ccfb6692
nbuendia <nicole.buendia424@gmail.com>
2021-12-25 01:16:15 -0500

    contact link component

---
+++ client/Components/Contact.js
@@ -0,0 +1,23 @@
+import { Box, Container, Typography } from "@mui/material";
+import React, {Component} from "react";
+
+class Contact extends Component {
+    render() {
+        return(
+            <>
+                <Container>
+                    <Box style={{marginTop: '20vh', height: '80vh', backgroundColor: '#000000', opacity: 0.6}}>
+                        <h1>CONTACT</h1>
+                        <h1>CONTACT</h1>
+                        <h1>CONTACT</h1>
+                        <Typography>
+                            CALL US!
+                        </Typography>
+                    </Box>
+                </Container>
+            </>
+        )
+    }
+}
+
+export default Contact;
