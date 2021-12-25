0b2665e2e96b47aa1a9c17416033f03887b093f9
nbuendia <nicole.buendia424@gmail.com>
2021-12-25 01:16:48 -0500

    event component skeleton

---
+++ client/Components/Events.js
@@ -0,0 +1,20 @@
+import { Box, Container } from "@mui/material";
+import React, {Component} from "react";
+
+class Events extends Component {
+    render() {
+        return(
+            <>
+                <Container>
+                    <Box style={{marginTop: '20vh', height: '80vh', backgroundColor: '#000000', opacity: 0.6}}>
+                        <h1>EVENTS</h1>
+                        <h1>EVENTS</h1>
+                        <h1>EVENTS</h1>
+                    </Box>
+                </Container>
+            </>
+        )
+    }
+}
+
+export default Events;
