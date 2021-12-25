4c408e285f102f43726a676264cccbc5c54497c2
nbuendia <nicole.buendia424@gmail.com>
2021-12-25 01:09:14 -0500

    added all paths needed for page

--- client/AllRoutes.js
+++ client/AllRoutes.js
@@ -3,15 +3,27 @@ import {Routes, Route} from 'react-router-dom';
 
 //ROUTES
 import Home from './Components/Home';
+import Contact from "./Components/Contact";
+import Events from "./Components/Events";
+import Food from "./Components/menus/Food";
+import Beer from "./Components/menus/Beer";
+import Cocktails from "./Components/menus/Cocktails";
+import SpiritsWines from "./Components/menus/SpiritsWines";
 
 class AllRoutes extends Component {
     render() {
         return(
-            <div>
+            <>
                 <Routes>
                     <Route path='/' element={<Home/>} />
+                    <Route path='/events' element={<Events/>} />
+                    <Route path='/contact' element={<Contact/>} />
+                    <Route path='/food' element={<Food/>} />
+                    <Route path='/beer' element={<Beer/>} />
+                    <Route path='/cocktails' element={<Cocktails/>} />
+                    <Route path='/spirits-wines' element={<SpiritsWines/>} />
                 </Routes>
-            </div>
+            </>
         )
     }
 }
