0cd9a5228b85a36973e0dcabb1ea45986ab92af4
nbuendia <nicole.buendia424@gmail.com>
2021-12-25 01:09:51 -0500

    updated homepage to display background img and nav bar

--- client/Components/Home.js
+++ client/Components/Home.js
@@ -1,9 +1,34 @@
+import { Box, Container, Typography } from "@mui/material";
+import { typography, width } from "@mui/system";
 import React, {Component} from "react";
 
 class Home extends Component {
     render() {
         return(
-            <h1>HELLO WORLD</h1>
+            <>
+                <Box style={{display: 'flex', justifyContent: 'space-between', textAlign: 'center'}}>
+                    {/* <Box style={{marginTop: '25vh', width: '23vw'}}>
+                        <Typography >
+                            246 ANYPLACE ST,
+                            12345 NEW YORK, NY
+                        </Typography>
+                        <br/>
+                        <Typography>
+                            <b><u>HOURS OF OPERATION</u></b><br/>
+                            MONDAY | 11am - 10pm <br/>
+                            TUESDAY | 11am - 10pm <br/>
+                            WEDNESDAY | 10am - 12am <br/>
+                            THURSDAY | 10am - 12am <br/>
+                            FRIDAY | 10am - 2am <br/>
+                            SATURDAY | 11am - 2am <br/>
+                            SUNDAY | 11am - 12am
+                        </Typography>
+                    </Box>
+                    <Box style={{marginTop: '25vh', width: '23vw'}}>
+                        MORE STUFF HERE
+                    </Box> */}
+                </Box>
+            </>
         )
     }
 }
