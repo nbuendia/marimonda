006b0fc53b42c964bec6b70ebab6b56fdbd528ed
nbuendia <nicole.buendia424@gmail.com>
2021-12-26 18:21:44 -0500

    changed from class ot func component, added withStyles MUI, added classes to BOX elems

--- client/Components/Home.js
+++ client/Components/Home.js
@@ -1,35 +1,45 @@
-import { Box} from "@mui/material";
-import React, {Component} from "react";
+import React from "react";
 
-class Home extends Component {
-    render() {
-        return(
-            <>
-                <Box style={{display: 'flex', justifyContent: 'space-between', textAlign: 'center'}}>
-                    {/* <Box style={{marginTop: '25vh', width: '23vw'}}>
-                        <Typography >
-                            246 ANYPLACE ST,
-                            12345 NEW YORK, NY
-                        </Typography>
-                        <br/>
-                        <Typography>
-                            <b><u>HOURS OF OPERATION</u></b><br/>
-                            MONDAY | 11am - 10pm <br/>
-                            TUESDAY | 11am - 10pm <br/>
-                            WEDNESDAY | 10am - 12am <br/>
-                            THURSDAY | 10am - 12am <br/>
-                            FRIDAY | 10am - 2am <br/>
-                            SATURDAY | 11am - 2am <br/>
-                            SUNDAY | 11am - 12am
-                        </Typography>
-                    </Box>
-                    <Box style={{marginTop: '25vh', width: '23vw'}}>
-                        MORE STUFF HERE
-                    </Box> */}
-                </Box>
-            </>
-        )
+//MATERIAL UI
+import { Box, Typography } from "@mui/material";
+import { withStyles } from '@mui/styles';
+
+const styles = {
+    container: {
+        marginTop: '20vh',
+        height: '80vh'
+    },
+    innerContainer: {
+        paddingTop: '10vh'
     }
+};
+
+function Home(props) {
+    const { classes } = props;
+
+    return(
+        <>
+            <Box className={classes.container}>
+                <Box className={classes.innerContainer}>
+                    <Typography>
+                        246 ANYPLACE ST,
+                        12345 NEW YORK, NY
+                    </Typography>
+                    <br/>
+                    <Typography>
+                        <b><u>HOURS OF OPERATION</u></b><br/>
+                        MONDAY | 11am - 10pm <br/>
+                        TUESDAY | 11am - 10pm <br/>
+                        WEDNESDAY | 10am - 12am <br/>
+                        THURSDAY | 10am - 12am <br/>
+                        FRIDAY | 10am - 2am <br/>
+                        SATURDAY | 11am - 2am <br/>
+                        SUNDAY | 11am - 12am
+                    </Typography>
+                </Box>
+            </Box>
+        </>
+    )
 }
 
-export default Home;
+export default withStyles(styles)(Home);
