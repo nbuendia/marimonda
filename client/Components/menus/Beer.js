aba3475ce9c1020ba3dd701f235777f019ec2887
nbuendia <nicole.buendia424@gmail.com>
2021-12-27 21:40:14 -0500

    finished beer component with tabs to filter beers

--- client/Components/menus/Beer.js
+++ client/Components/menus/Beer.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 //MATERIAL UI
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grid, Tab, Tabs, Typography } from "@mui/material";
+
+//COMPONENTS
+import Cans from "./beers/Cans";
+import Bottles from "./beers/Bottles";
+import Drafts from "./beers/Drafts";
 
 function Beer() {
+    const [value, setValue] = useState('can');
+    function handleChange(event, newValue) {
+        setValue(newValue)
+    }
+
     return(
         <>
             <Container className='container'>
                 <Box className='inner-container'>
-                    <Typography>BEER</Typography>
+                    <Tabs centered variant="fullWidth" value={value} onChange={handleChange}>
+                        <Tab label='CANS' value='can' />
+                        <Tab label='BOTTLES' value='bottle' />
+                        <Tab label='DRAFTS'value='draft' />
+                    </Tabs>
+                    <Grid container spacing={1} justifyContent={'center'}>
+                        {value === 'can' ? <Cans />
+                        : value === 'bottle' ? <Bottles />
+                        : <Drafts />}
+                    </Grid>
                 </Box>
             </Container>
         </>
