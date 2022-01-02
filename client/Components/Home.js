2ff1b3ed204eb74a1ac0a2974511ce7532221dae
nbuendia <nicole.buendia424@gmail.com>
2022-01-01 21:50:55 -0500

    updated home page with address, opeing hours and lunch specials - NOT DONE *add hh*

--- client/Components/Home.js
+++ client/Components/Home.js
@@ -1,45 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //MATERIAL UI
-import { Box, Container, Typography } from "@mui/material";
-import { withStyles } from '@mui/styles';
+import { Box, Button, Container, IconButton, Typography } from "@mui/material";
 
-const styles = {
-    container: {
-        marginTop: '20vh',
-        height: '80vh'
-    },
-    innerContainer: {
-        paddingTop: '10vh'
-    }
-};
+//COMPONENTS
+import Address from "./home/Address";
+import Schedule from "./home/Schedule";
+import DailySpecials from "./home/DailySpecials";
 
-function Home(props) {
-    const { classes } = props;
-
+function Home() {
     return(
         <>
-            <Container className={classes.container}>
-                <Box className={classes.innerContainer}>
-                    <Typography>
-                        246 ANYPLACE ST,
-                        12345 NEW YORK, NY
-                    </Typography>
-                    <br/>
-                    <Typography>
-                        <b><u>HOURS OF OPERATION</u></b><br/>
-                        MONDAY | 11am - 10pm <br/>
-                        TUESDAY | 11am - 10pm <br/>
-                        WEDNESDAY | 10am - 12am <br/>
-                        THURSDAY | 10am - 12am <br/>
-                        FRIDAY | 10am - 2am <br/>
-                        SATURDAY | 11am - 2am <br/>
-                        SUNDAY | 11am - 12am
-                    </Typography>
+            <Container className='container'>
+                <Box className='address-container'>
+                    <Address />
+                </Box>
+                <Box className="flex-row" style={{justifyContent: 'space-between', textAlign: 'center'}}>
+                    <Box>
+                        <Schedule />
+                    </Box>
+                    <Box>
+                        <DailySpecials />
+                    </Box>
                 </Box>
             </Container>
         </>
     )
 }
 
-export default withStyles(styles)(Home);
+export default Home;
