fa98c570a10ee7ae9a645f4a3d61c1135d87573b
nbuendia <nicole.buendia424@gmail.com>
2022-01-02 19:30:54 -0500

    HH component

---
+++ client/Components/home/HappyHour.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+//MATERIAL UI
+import { Box, IconButton, Typography } from "@mui/material";
+//MATERIAL UI ICONS
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+
+
+function HappyHour() {
+    const [showHappyHour, setShowHappyHour] = useState(false);
+
+    function handleChangeHappyHour() {
+        setShowHappyHour(!showHappyHour);
+    }
+
+    return(
+        <>
+            {showHappyHour ? 
+                <Box>
+                    <Box className="flex-row alignment" justifyContent={'center'}>
+                        <Typography className="blur" fontSize={20}>
+                            <b><u>HAPPY HOUR</u></b>
+                        </Typography>
+                        <IconButton className="blur" onClick={handleChangeHappyHour}>
+                            <ExpandLessIcon className="button" />
+                        </IconButton>
+                    </Box>
+                    <Typography className="blur">
+                        <span style={{fontSize: '14px'}}>MON - FRI <b>|</b> 4pm - 7pm</span><br/>
+                        <b>$2 OFF ALL DRAFTS</b><br/>
+                        <b>$8 REFAJO PITCHERS</b><br/>
+                        <b>$7 MOJITO & SANGRIAS</b><br/>
+                        <b>$3 AGUARDIENTE SHOTS</b>
+                    </Typography>
+                </Box> :
+                <Box className="flex-row alignment" justifyContent={'center'}>
+                    <Typography className="blur" fontSize={20}>
+                        <b>HAPPY HOUR</b>
+                    </Typography>
+                    <IconButton className="blur" onClick={handleChangeHappyHour}>
+                        <KeyboardArrowDownIcon className="button" />
+                    </IconButton>
+                </Box>
+            }
+        </>
+    )
+}
+
+export default HappyHour;
