8c046d342e1d481d53ddd3ea661283791c0bb1fe
nbuendia <nicole.buendia424@gmail.com>
2022-01-06 17:52:36 -0500

    added a photo component that will display picures of resturant and events - MUST FIND IMGS

--- client/Components/home/Photos.js
+++ client/Components/home/Photos.js
@@ -1,36 +1,49 @@
 import React, { useState } from "react";
 
 //MATERIAL UI
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Paper } from "@mui/material";
 //MATERIAL UI ICONS
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-import data from '../menus/menuData/dailySpecials.json';
+import data from '../photos/homePhotos.json';
 
 
 function Photos() {
-    const [showPhotos, setShowPhotos] = useState(false);
+    const photoArray = data.map((item) => item.img);
+    const photoDescription = data.map((item) => item.description);
+    const [currPhoto, setCurrPhoto] = useState(0);
 
-    function handleChangePhotos() {
-        setShowPhotos(!showPhotos);
+    let lastPhotoIdx = photoArray.length - 1;
+    let currentPhoto = photoArray[currPhoto];
+
+    function handlePrevPhoto() {
+        if (currPhoto === 0) setCurrPhoto(lastPhotoIdx);
+        else setCurrPhoto(currPhoto - 1);
+    }
+
+    function handleNextPhoto() {
+        if (currPhoto === lastPhotoIdx) setCurrPhoto(0);
+        else setCurrPhoto(currPhoto + 1);
     }
 
     return(
-        <>
-            {/* PHOTOS */}
-            <Box style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                {/* <img src="https://i.imgur.com/hTdVE68m.jpg" className="test"/> */}
-                <IconButton>
-                    <ArrowBackIosNewIcon />
+        <Box className="flex-row alignment photo-container">
+            <Box className="inner-photo-container" width={'25vw'}>
+                <IconButton onClick={handlePrevPhoto}>
+                    <ArrowBackIosNewIcon className="button"/>
                 </IconButton>
-                <img src="https://i.imgur.com/hTdVE68m.jpg"/>
-                <IconButton>
-                    <ArrowForwardIosIcon />
+                <img src={currentPhoto}/>
+                <IconButton onClick={handleNextPhoto}>
+                    <ArrowForwardIosIcon className="button" />
                 </IconButton>
-                {/* <img src="https://i.imgur.com/hTdVE68m.jpg"/> */}
             </Box>
-        </>
+            <Box width={'25vw'}>
+                <Paper className="photo-description">
+                    {photoDescription[currPhoto]}
+                </Paper>
+            </Box>
+        </Box>
     )
 }
 
