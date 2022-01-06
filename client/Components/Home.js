2dfa6e8fe2c549389ace920428f6c376e7fef7ef
nbuendia <nicole.buendia424@gmail.com>
2022-01-05 21:05:44 -0500

    changed layout to have info when scrolled down rather than over image, started a photo cmponent to add to home

--- client/Components/Home.js
+++ client/Components/Home.js
@@ -8,27 +8,28 @@ import Address from "./home/Address";
 import Schedule from "./home/Schedule";
 import DailySpecials from "./home/DailySpecials";
 import HappyHour from "./home/HappyHour";
+import Photos from "./home/Photos";
 
 function Home() {
     return(
         <>
-            <Container className='container'>
-                <Box className='address-container'>
+            <Container style={{height: '95vh'}} />
+            <Container className="home-info-container">
+                <hr className="hr-splitter" style={{marginBottom: '25px'}}/>
+                <Box>
                     <Address />
                 </Box>
-                <Box className="flex-row" justifyContent={'space-between'} textAlign={'center'}>
-                    <Box width={250}>
-                        <Schedule />
-                    </Box>
-                    <Box width={250}>
-                        <Box height={200}>
-                            <DailySpecials />
-                        </Box>
-                        <Box>
-                            <HappyHour />
-                        </Box>
-                    </Box>
+                <hr className="hr-address-splitter" />
+                <Box className="info-inner-container">
+                    <Schedule />
+                    <DailySpecials />
+                    <HappyHour />
                 </Box>
+            </Container>
+            <hr className="hr-splitter"/>
+            <Container className="blur" style={{paddingTop: '25px', marginBottom: '5vh'}} >
+                <Photos />
+                <hr className="hr-splitter" style={{marginTop: '25px'}}/>
             </Container>
         </>
     )
