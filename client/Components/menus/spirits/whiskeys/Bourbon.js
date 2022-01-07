637a965b01cb6ff25560ba7b0744a467ce5ab8da
nbuendia <nicole.buendia424@gmail.com>
2022-01-07 16:15:15 -0500

    bourbon component that displays a list of bourbons with corresponfing price

---
+++ client/Components/menus/spirits/whiskeys/Bourbon.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+//MATERIAL UI
+import { Box,Grid, Paper, Typography } from "@mui/material";
+
+import data from '../../menuData/spirits.json';
+
+function Bourbon() {
+    const bourbons = data.filter((spirit) => spirit.category === 'bourbon whiskey');
+
+    return(
+        <>
+            <Grid item xs={6}>
+                <Typography fontSize={20} fontWeight={'bold'} borderBottom={2} marginLeft={'25px'}>
+                    NAME
+                </Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography fontSize={20} fontWeight={'bold'} borderBottom={2} textAlign={'right'} marginRight={'25px'}>
+                    PRICE
+                </Typography>
+            </Grid>
+            {bourbons.map((whiskey) => (
+                <Grid item xs={12} key={whiskey.id}>
+                    <Paper elevation={0} className="spirit-paper-container" style={{backgroundColor: 'transparent'}}>
+                        <Box>
+                            <Typography fontSize={18}>
+                                {whiskey.name}
+                            </Typography>
+                        </Box>
+                        <Typography fontSize={18}>
+                            ${whiskey["neat-price"]}
+                        </Typography>
+                    </Paper>
+                </Grid>
+            ))}
+        </>
+    )
+}
+
+export default Bourbon;
