3eae829b8f8075dc98887e30bb11d646b26993a1
nbuendia <nicole.buendia424@gmail.com>
2022-01-22 22:49:45 -0500

    imported react calendar styling

--- client/App.js
+++ client/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import 'react-calendar/dist/Calendar.css';
 
 //COMPONENTS
 import AllRoutes from './AllRoutes';
